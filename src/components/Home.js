0c3b58fb4142fea6a8790f923f6a37c1b714294a
Anna Spysz <anna.spysz@stackery.io>
2018-06-13 13:43:49 -0700

    Added links

--- src/components/Home.js
+++ src/components/Home.js
@@ -36,7 +36,7 @@ class Home extends Component {
           </div>
           <div className='space' />
           <h2 className='center-align'>Join us in Portland!</h2>
-          <p>ServerlessDays Portland will take place at the Armory in downtown Portland, Oregon.</p>
+          <p>ServerlessDays Portland will take place at <a href='https://www.pcs.org/' target='_blank'>The Armory</a> in downtown Portland, Oregon.</p>
           <p>Portland Center Stage at The Armory <br />128 NW 11th Ave <br />Portland, OR 97209</p>
           <div className='space' />
         </div>
