0c539272282a91f68a06a849e82d389bd629c377
Anna Spysz <anna.spysz@stackery.io>
2018-06-13 10:24:19 -0700

    Cleaning up CSS, styled footer

--- src/components/Foot.js
+++ src/components/Foot.js
@@ -7,6 +7,7 @@ function Foot () {
       <Footer copyrights='© 2018 Serverless Days Portland'
         links={
           <ul>
+            <li>Links can go here</li>
             {/* add optional links here
               <li><a className='grey-text text-lighten-3 left-align' href='#!'>Link 1</a></li>
               <li><a className='grey-text text-lighten-3 left-align' href='#!'>Link 2</a></li>
@@ -18,7 +19,7 @@ function Foot () {
         className='copyrights'
       >
         <h5 className='white-text left-align'>Footer Content</h5>
-        <p className='grey-text text-lighten-4 left-align'>Footer content will go here.</p>
+        <p className='grey-text text-lighten-4 left-align'>Lorem ipsum dolor amet pinterest cardigan pork belly, cornhole listicle vape lumbersexual veniam excepteur ut. Voluptate unicorn health goth copper mug DIY tote bag gentrify ut. Ea pariatur mlkshk sartorial cred microdosing messenger bag food truck. Put a bird on it est photo booth nisi laborum letterpress, migas plaid gochujang cliche. Taxidermy pinterest plaid eiusmod.</p>
       </Footer>
     </footer>
   );
