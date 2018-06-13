a49da7c961cc81a47e976851fa8e2fb87214adaa
Anna Spysz <anna.spysz@stackery.io>
2018-06-13 16:05:49 -0700

    Minor changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,17 +11,17 @@ class Home extends Component {
           <div className='valign-wrapper'>
             <Row>
               <Col s={12} m={4} l={4} className='grid-example center-align top-marg'>
-                <img src={'./media/unicorn_mascot.png'} alt={'ServerlessDays PDX logo'} className='responsive-img not-wide' />
+                <img src={'./media/unicorn_mascot.png'} alt={'ServerlessDays mascot'} className='responsive-img not-wide' />
                 <h4 className='top-marg'>Workshops</h4>
                 <p>Hone your cloud skills and build interesting applications in one or more of the serverless workshops being held one day prior to the conference.</p>
               </Col>
               <Col s={12} m={4} l={4} className='grid-example center-align top-marg'>
-                <img src={'./media/anteater_mascot.png'} alt={'ServerlessDays PDX logo'} className='responsive-img not-wide' />
+                <img src={'./media/anteater_mascot.png'} alt={'ServerlessDays mascot'} className='responsive-img not-wide' />
                 <h4 className='top-marg'>Serverless for everyone</h4>
                 <p>The single day, single track conference for everyone where real users showcase their solution to common problems using serverless platforms.</p>
               </Col>
               <Col s={12} m={4} l={4} className='grid-example center-align top-marg'>
-                <img src={'./media/moth_mascot.png'} alt={'ServerlessDays PDX logo'} className='responsive-img not-wide' />
+                <img src={'./media/moth_mascot.png'} alt={'ServerlessDays mascot'} className='responsive-img not-wide' />
                 <h4 className='top-marg'>Join the community</h4>
                 <p>Whether you are a beginner or an expert, meet like minded-people and build your network in the global cloud society.</p>
               </Col>
