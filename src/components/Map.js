a1cfa235bdecbff6fd8a54608cdff397bc7ab410
Anna Spysz <anna.spysz@stackery.io>
2018-06-13 13:35:34 -0700

    Added map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,53 @@
 import React from 'react';
+import { compose, withProps } from 'recompose';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
-function Map () {
-  return (
-    <div>
-      A map will go here!
-    </div>
-  );
+const MapComponent = compose(
+  withProps({
+    googleMapURL: 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places',
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `60vh` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withScriptjs,
+  withGoogleMap
+)((props) =>
+  <GoogleMap
+    defaultZoom={14}
+    defaultCenter={{ lat: 45.524039, lng: -122.681709 }}
+  >
+    {props.isMarkerShown && <Marker position={{ lat: 45.524039, lng: -122.681709 }} onClick={props.onMarkerClick} />}
+  </GoogleMap>
+);
+
+class Map extends React.PureComponent {
+  state = {
+    isMarkerShown: false,
+  }
+
+  componentDidMount() {
+    this.delayedShowMarker()
+  }
+
+  delayedShowMarker = () => {
+    setTimeout(() => {
+      this.setState({ isMarkerShown: true })
+    }, 3000)
+  }
+
+  handleMarkerClick = () => {
+    this.setState({ isMarkerShown: false })
+    this.delayedShowMarker()
+  }
+
+  render() {
+    return (
+      <MapComponent
+        isMarkerShown={this.state.isMarkerShown}
+        onMarkerClick={this.handleMarkerClick}
+      />
+    )
+  }
 }
 
 export default Map;
