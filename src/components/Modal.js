c5ff715e65727c27932150b28a02236ae1290fe4
Anna Spysz <anna@annaspysz.com>
2018-06-19 14:40:08 -0700

    Temporarily disabling modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,8 +2,9 @@ import React from 'react';
 import Signup from './Signup';
 
 const Modal = () => {
+  {/* <div id='signupModal' className='modal'> */}
   return (
-    <div id='signupModal' className='modal'>
+    <div id='modal' className='modal'>
       <div className='modal-content'>
         <div className='center-align'>
           <img src={'./media/logo_serverless_PDX.png'} alt={'ServerlessDays PDX logo'} className='responsive-img' />
