31a3dcefae496b6e16f69262f5abec8f4df361dc
Anna Spysz <anna@annaspysz.com>
2018-06-19 12:09:53 -0700

    Added API key

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { compose, withProps } from 'recompose';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import API_KEY from './.secrets'
 
 const MapComponent = compose(
   withProps({
-    googleMapURL: 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places',
+    googleMapURL: `https://maps.googleapis.com/maps/api/js?v=3.exp&key=${API_KEY}&libraries=geometry,drawing,places`,
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `60vh` }} />,
     mapElement: <div style={{ height: `100%` }} />,
