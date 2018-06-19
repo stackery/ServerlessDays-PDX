d4a8d95dd1ebc8a645caed4a494acd515d079bf6
Anna Spysz <anna@annaspysz.com>
2018-06-18 17:12:36 -0700

    Gave the header a bit more space

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ class Header extends Component {
           <img src={'./media/logo_serverless_PDX.png'} alt={'ServerlessDays PDX logo'} className='responsive-img' />
           <h2>One Day. One Track. One Community.</h2>
           <h4>Save The Date</h4>
-          <a href='https://www.papercall.io/serverlessdayspdx' target='_blank' className='btn-large'>Propose a talk!</a>
+          <a href='https://www.papercall.io/serverlessdayspdx' target='_blank' className='btn-large top-marg'>Propose a talk!</a>
           <h5 className='top-marg'>September 17, 2018 - Portland, Oregon</h5>
         </header>
       </div>
