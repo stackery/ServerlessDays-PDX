963b6f787c6de09440d2d2675b9e163eb5de9325
Anna Spysz <anna@annaspysz.com>
2018-06-25 15:48:39 -0700

    Updated ticket link

--- src/components/Foot.js
+++ src/components/Foot.js
@@ -11,7 +11,7 @@ function Foot () {
           <div>
             <div className='space-foot hide-on-med-and-down' />
             <ul className='left-align'>
-              <li><a data-target='signupModal' className='modal-trigger grey-text text-lighten-3 bold'>Tickets</a></li>
+              <li><a href='https://www.eventbrite.com/e/serverlessdays-pdx-tickets-47446178798' target='_blank' rel='noopener noreferrer'>Tickets</a></li>
               <li><Scrollchor animate={{offset: -100, duration: 500}} to='#top'><Link className='grey-text text-lighten-3' to='/about'>About</Link></Scrollchor></li>
               <li><Link className='grey-text text-lighten-3' to='/location#venue'>Venue</Link></li>
               <li><Scrollchor animate={{offset: -100, duration: 500}} to='#top'><Link className='grey-text text-lighten-3' to='/speakers'>Speakers</Link></Scrollchor></li>
