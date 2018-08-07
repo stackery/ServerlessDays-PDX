fe5da53b186c6cdb5fd9c0bd8a75d266ed92c7fe
Anna Spysz <anna@annaspysz.com>
2018-08-07 15:23:55 -0700

    Updating location

--- src/components/Location.js
+++ src/components/Location.js
@@ -13,7 +13,7 @@ class Location extends Component {
           <div className='container'>
             <h2 className='center-align' id='venue'>Venue</h2>
             <p>ServerlessDays Portland will take place at <a href='https://www.pcs.org/' target='_blank' rel='noopener noreferrer'>The Armory</a> in downtown Portland, Oregon.</p>
-            <p>Portland Center Stage at The Armory <br />128 NW 11th Ave <br />Portland, OR 97209</p>
+            <p>Ellen Bye Studio at The Armory <br />128 Northwest 11th Avenue <br />Portland, OR 97209</p>
             <div className='small-space' />
           </div>
         </div>
